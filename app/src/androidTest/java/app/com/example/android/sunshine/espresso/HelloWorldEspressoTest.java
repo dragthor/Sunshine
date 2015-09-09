0cb10524fe67176abf191af812c59b597db416b6
Kristofer Krause <kris.krause@gmail.com>
2015-09-09 05:27:04 -0400

    attempt esspresso

---
+++ app/src/androidTest/java/app/com/example/android/sunshine/espresso/HelloWorldEspressoTest.java
@@ -0,0 +1,31 @@
+package app.com.example.android.sunshine.espresso;
+
+//import android.support.test.rule.ActivityTestRule;
+//import android.support.test.runner.AndroidJUnit4;
+//import android.test.suitebuilder.annotation.LargeTest;
+
+//import org.junit.Rule;
+//import org.junit.runner.RunWith;
+
+//import static android.support.test.espresso.Espresso.onView;
+//import static android.support.test.espresso.matcher.ViewMatchers.*;
+//import static android.support.test.espresso.assertion.ViewAssertions.*;
+
+import app.com.example.android.sunshine.MainActivity;
+import app.com.example.android.sunshine.R;
+
+/**
+ * Created by kkrause on 9/7/2015.
+ */
+//@RunWith(AndroidJUnit4.class)
+//@LargeTest
+//public class HelloWorldEspressoTest {
+
+    //@Rule
+    //public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    //public void testListGoesOverTheFold() {
+        //onView(withId(R.id.listview_forecast)).check(matches(isDisplayed()));
+        //onView(withText("Hello world!")).check(matches(isDisplayed()));
+    //}
+//}
